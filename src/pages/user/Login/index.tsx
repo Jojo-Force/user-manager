c5bb5ad141c0753700e64f8ec39bab758907c2ef
jojo <jojo@duolin.com>
2025-10-30 15:36:52 +0800

    1.完成登录功能的前端开发

--- src/pages/user/Login/index.tsx
+++ src/pages/user/Login/index.tsx
@@ -54,8 +54,8 @@ const Login: React.FC = () => {
   const handleSubmit = async (values: API.LoginParams) => {
     try {
       // 登录
-      const msg = await login({ ...values, type });
-      if (msg.status === 'ok') {
+      const user = await login({ ...values, type });
+      if (user) {
         const defaultLoginSuccessMessage = intl.formatMessage({
           id: 'pages.login.success',
           defaultMessage: '登录成功！',
@@ -69,9 +69,8 @@ const Login: React.FC = () => {
         history.push(redirect || '/');
         return;
       }
-      console.log(msg);
       // 如果失败去设置用户错误信息
-      setUserLoginState(msg);
+      setUserLoginState(user);
     } catch (error) {
       const defaultLoginFailureMessage = intl.formatMessage({
         id: 'pages.login.failure',
@@ -113,7 +112,7 @@ const Login: React.FC = () => {
           {type === 'account' && (
             <>
               <ProFormText
-                name="username"
+                name="userAccount"
                 fieldProps={{
                   size: 'large',
                   prefix: <UserOutlined className={styles.prefixIcon} />,
@@ -135,7 +134,7 @@ const Login: React.FC = () => {
                 ]}
               />
               <ProFormText.Password
-                name="password"
+                name="userPassword"
                 fieldProps={{
                   size: 'large',
                   prefix: <LockOutlined className={styles.prefixIcon} />,
@@ -145,6 +144,12 @@ const Login: React.FC = () => {
                   defaultMessage: '密码: ant.design',
                 })}
                 rules={[
+                  {
+                    min: 8,
+                    type: 'string',
+                    required: true,
+                    message: '密码长度小于8',
+                  },
                   {
                     required: true,
                     message: (
